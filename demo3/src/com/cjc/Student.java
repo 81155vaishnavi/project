a65e3676d9d568dd698b6ef91e589e80c8c307c8
admin <admin@DESKTOP-IDFE2KR>
2024-09-20 16:32:00 +0530

    by VAISHU2

---
+++ demo3/src/com/cjc/Student.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class Student {
+
+	public Student()
+	{
+		System.out.println("THIS IS CONSTRUCTOR");
+	}
+	
+	public void display()
+	{
+		System.out.println("*******************");
+	}
+}
